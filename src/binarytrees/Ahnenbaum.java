059cec11b4310805019204cdadea0b721286a9c2
bananentee <ibesven@gmail.com>
2024-08-29 15:25:59 +0200

    preOrderList implemented

--- src/binarytrees/Ahnenbaum.java
+++ src/binarytrees/Ahnenbaum.java
@@ -12,16 +12,32 @@ public class Ahnenbaum {
     BinaryTree<String> rrw;
     BinaryTree<String> lw;
     BinaryTree<String> rw;
-    BinaryTree<String> svensAhnenbaum;
+    BinaryTree<String> lisasAhnenbaum;
 
     Ahnenbaum() {
-        llw = new BinaryTree<>("Katherina Ibe");
-        lrw = new BinaryTree<>("Viktor Fedossimov");
-        rlw = new BinaryTree<>("Ela Chernega-Pokrovsky");
-        rrw = new BinaryTree<>("Vater von Igor Ibe");
-        lw = new BinaryTree<>("Natalie Ibe",llw,lrw);
-        rw = new BinaryTree<>("Igor Ibe",rlw,rrw);
-        svensAhnenbaum = new BinaryTree<>("Sven Ibe",lw,rw);
+        llw = new BinaryTree<>("Jacqueline Bouvier");
+        lrw = new BinaryTree<>("Clancy Bouvier");
+        rlw = new BinaryTree<>("Mona Simpson");
+        rrw = new BinaryTree<>("Abraham J. Simpson");
+        lw = new BinaryTree<>("Marge Simpson",llw,lrw);
+        rw = new BinaryTree<>("Homer Simpson",rlw,rrw);
+        lisasAhnenbaum = new BinaryTree<>("Lisa Simpson",lw,rw);
+
+        preOrderAusgabe();
+    }
+
+    public void preOrderAusgabe() {
+        search(lisasAhnenbaum);
+    }
+
+    private void search(BinaryTree<String> b) {
+        System.out.println(b.getContent());
+        if (!b.getLeftTree().isEmpty()) {
+            search(b.getLeftTree());
+        }
+        if (!b.getRightTree().isEmpty()) {
+            search(b.getRightTree());
+        }
     }
 
 
